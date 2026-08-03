d59627e267842e06aa33e22cbf3aeed31b65951b
Elio Ucan <eliozapata2006@gmail.com>
2026-08-03 16:47:26 -0600

    commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts array
+ * @array: array to sort
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+size_t i, j;
+int temp;
+if (array == NULL || size < 2)
+return;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+}
+}
+}
+}
