290d0da014d9e5e239f7d4a9c9f4feabfd40ee1c
Elio Ucan <eliozapata2006@gmail.com>
2026-08-03 16:56:04 -0600

    commit

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts list
+ * @list: pointer to head pointer
+ */
+void insertion_sort_list(listint_t **list)
+{
+listint_t *curr, *prev, *next;
+if (list == NULL || *list == NULL || (*list)->next == NULL)
+return;
+curr = (*list)->next;
+while (curr != NULL)
+{
+next = curr->next;
+prev = curr->prev;
+while (prev != NULL && prev->n > curr->n)
+{
+prev->next = curr->next;
+if (curr->next != NULL)
+curr->next->prev = prev;
+curr->prev = prev->prev;
+curr->next = prev;
+if (prev->prev != NULL)
+prev->prev->next = curr;
+else
+*list = curr;
+prev->prev = curr;
+print_list(*list);
+prev = curr->prev;
+}
+curr = next;
+}
+}
